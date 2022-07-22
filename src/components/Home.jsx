796337ddd428f310f3c8da3526bfc4111c070265
Suwipathuesayom <suwipa.thu@gmail.com>
2022-07-23 02:34:17 +0700

    commit react-tailwind

---
+++ src/components/Home.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import HeroImage from "../assets/herolmage.jpg";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import Typewriter from "typewriter-effect";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full sm:mt-20">
+          <h2 className="text-4xl sm:text-5xl  font-bold text-yellow-300 mt-20">
+            <Typewriter
+              options={{
+                autoStart: true,
+                loop: true,
+                delay: 50,
+                strings: ["My name's Suwipa"],
+              }}
+            />
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            I'm have 4 months of experience in frontend development. Currently,
+            I love to work on web applications using technologies like React,
+            Tailwind, Bootstrap, MaterialUI.
+          </p>
+          <div>
+            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            src={HeroImage}
+            alt="my profile"
+            className="rounded-2xl  mx-auto w-2/3 mt-10 sm:w-1/2 sm:mr-auto"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+
+const SocialLinks = () => {
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkedIn <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/%E0%B8%AA%E0%B8%B8%E0%B8%A7%E0%B8%B4%E0%B8%A0%E0%B8%B2-%E0%B8%96%E0%B8%B7%E0%B8%AD%E0%B8%AA%E0%B8%A2%E0%B8%A1-962318225/",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          Github <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/Suwipathuesayom?tab=repositories",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Email <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mailto:suwipa.thu@gmail.com",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Resume <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: "/public/Resume.pdf",
+      style: "rounded-tr-md",
+      download: true,
+    },
+  ];
+  return (
+    <div className="lg:flex flex-col top-[35%] left-0 fixed ">
+      <ul>
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-sm duration-300 bg-gray-500" +
+              " " +
+              style
+            }
+          >
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SocialLinks;
